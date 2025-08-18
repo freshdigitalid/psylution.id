68bb957d494db001d0d34f412b98cc6393a42fd0
Muhammad Fahri Ramadhan <00.fahri.r@gmail.com>
2025-08-18 19:20:06 +0700

    fix: Use controller to redirect to dashboard (#18)

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\PsychologistController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('home/index');
-})->name('home');
+Route::get('/', [Controller::class, 'home'])
+    ->name('home');
 
 Route::get('/cari-psikolog', [PsychologistController::class, 'index'])
     ->name('psychologist.find');
@@ -17,5 +17,15 @@ Route::get('/psikolog/{id}', [PsychologistController::class, 'show'])
 Route::get('/psikolog-terbaik', function () {
     return Inertia::render('psychologist/best/index');
 })->name('psychologist.best');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [Controller::class, 'dashboard'])
+        ->name('dashboard');
+
+    Route::get('/profile', [Controller::class, 'profile'])
+        ->name('profile');
+
+});
 
 require __DIR__ . '/auth.php';
