201dd3c6357be8df912bdfacf5328a09630f0a59
fahri-r <00.fahri.r@gmail.com>
2025-09-27 20:56:24 +0700

    feat: Add available time of psychologist

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Enums\AppointmentStatus;
 use App\Models\Appointment;
 use App\Models\Psychologist;
+use App\Models\Schedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
@@ -22,10 +24,46 @@ class AppointmentController extends Controller
             ->findOrFail($psychologist_id);
             
         $psychologist->specializations->each->makeHidden('pivot');
+        
+        $today = Carbon::today('Asia/Jakarta')->setTimezone('UTC'); // current date (00:00:00)
+
+        // get schedules for today + that psychologist
+        $schedule = Schedule::where('psychologist_id', $psychologist_id)
+            ->whereDate('start_time', $today)
+            ->first();
+
+        $slots = collect();
+        if($schedule) {
+            $start = Carbon::parse($schedule->start_time);
+            $end   = Carbon::parse($schedule->end_time);
+
+            $break_start = $schedule->break_start_time ? Carbon::parse($schedule->break_start_time) : null;
+            $break_end   = $schedule->break_end_time ? Carbon::parse($schedule->break_end_time) : null;
+    
+            while ($start < $end) {
+                $next = (clone $start)->addHour();
+    
+                // skip break
+                if ($break_start && $break_end) {
+                    if ($start >= $break_start && $next <= $break_end) {
+                        $start = $next;
+                        continue;
+                    }
+                }
+    
+                $slots->push([
+                    'start_time'      => $start,
+                    'end_time'        => $next,
+                ]);
+    
+                $start = $next;
+            }
+        }
 
         return Inertia::render('psychologist/book/index', [
             'psychologist' => $psychologist,
             'patient' => $user->person,
+            'schedules' => $slots
         ]);
     }
 
