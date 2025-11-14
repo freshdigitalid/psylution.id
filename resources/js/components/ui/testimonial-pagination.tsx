59fdf51a5dd31ba681e1f37d310e87ac01ac4446
iqbaalynsyh234 <iqbalalyansyah3@gmail.com>
2025-11-14 11:18:11 +0700

    delete pagination fix

--- resources/js/components/ui/testimonial-pagination.tsx
+++ resources/js/components/ui/testimonial-pagination.tsx
@@ -4,8 +4,6 @@ import {
   PaginationContent,
   PaginationItem,
   PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
   PaginationEllipsis,
 } from '@/components/ui/pagination';
 
@@ -73,19 +71,6 @@ export const TestimonialPagination: React.FC<TestimonialPaginationProps> = ({
   return (
     <Pagination className="mt-8">
       <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              if (currentPage > 1) {
-                onPageChange(currentPage - 1);
-              }
-            }}
-            className={currentPage <= 1 ? 'pointer-events-none opacity-50' : ''}
-          />
-        </PaginationItem>
-
         {getVisiblePages().map((page, index) => (
           <PaginationItem key={`page-${typeof page === 'string' ? `ellipsis-${index}` : page}`}>
             {page === '...' ? (
@@ -104,19 +89,6 @@ export const TestimonialPagination: React.FC<TestimonialPaginationProps> = ({
             )}
           </PaginationItem>
         ))}
-
-        <PaginationItem>
-          <PaginationNext
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              if (currentPage < totalPages) {
-                onPageChange(currentPage + 1);
-              }
-            }}
-            className={currentPage >= totalPages ? 'pointer-events-none opacity-50' : ''}
-          />
-        </PaginationItem>
       </PaginationContent>
     </Pagination>
   );
